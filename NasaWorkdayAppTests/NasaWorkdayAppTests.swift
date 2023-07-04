f5dd0e6ddd06352739c821cc3528126bdc329fa8
Piyushmanghani7 <piyush.manghani10@gmail.com>
2023-07-03 23:30:25 -0500

    Write test cases using XCTest for fetching API data.

--- NasaWorkdayAppTests/NasaWorkdayAppTests.swift
+++ NasaWorkdayAppTests/NasaWorkdayAppTests.swift
@@ -9,28 +9,38 @@ import XCTest
 @testable import NasaWorkdayApp
 
 final class NasaWorkdayAppTests: XCTestCase {
-
+    
+    var sut: RestAPI!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = RestAPI()
+        
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+    
+    
+    
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        // we are using XCTestExpectation to be able to wait untill finishing
+        let expection = XCTestExpectation(description: " API data is fetching properly")
+        
+        
+        var ItemModelReponse: [Collection.collection_model.Items_model]?
+        var responseError: Error?
+        sut.SearchFetch(SearchName: "Mars", Page: 1) { (itemmodel, error) in
+            
+            ItemModelReponse = itemmodel
+            responseError = error
+            expection.fulfill()
         }
+        wait(for: [expection], timeout: 1)
+        
+        XCTAssertNotNil(ItemModelReponse) // Make sure that Items response != nil
+        XCTAssertNil(responseError)// Make sure that error == nil
+        
     }
-
+    
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
 }
