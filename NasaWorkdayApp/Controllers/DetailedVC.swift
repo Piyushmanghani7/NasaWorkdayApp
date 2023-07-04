6baeb5ff5f265358545b5c006c2e536dc654896d
Piyushmanghani7 <piyush.manghani10@gmail.com>
2023-07-03 23:57:53 -0500

    Create Detail screen on storyboard and make outlets of all component on DetailVC file.

---
+++ NasaWorkdayApp/Controllers/DetailedVC.swift
@@ -0,0 +1,26 @@
+//
+//  DetailedVC.swift
+//  NasaWorkdayApp
+//
+//  Created by Piyush Manghani on 7/3/23.
+//
+
+import UIKit
+
+class DetailedVC: UIViewController {
+    @IBOutlet weak var detailImage: UIImageView!
+    
+    @IBOutlet weak var date_lbl: UILabel!
+    @IBOutlet weak var location_lbl: UILabel!
+ 
+    @IBOutlet weak var description_lbl: UILabel!
+   
+    @IBOutlet weak var title_lbl: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
